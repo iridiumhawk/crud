1ecba00a0570ba59e84d6b5a12b71049991fe238
iridiumhawk <hawk.tmn@gmail.com>
2017-05-16 00:23:47 +0500

    comment

--- src/main/java/com/cherkasov/service/EntityService.java
+++ src/main/java/com/cherkasov/service/EntityService.java
@@ -6,10 +6,42 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface EntityService {
+    /**
+     *
+     * @param entity
+     */
     void save(Entity entity);
+
+    /**
+     *
+     * @param id
+     */
     void delete(int id);
+
+    /**
+     *
+     * @param entity
+     */
     void update(Entity entity);
+
+    /**
+     *
+     * @param id
+     * @return Entity
+     */
     Entity get(int id);
+
+    /**
+     *
+     * @return
+     * @throws SQLException
+     */
     List<Entity> getAll() throws SQLException;
+
+    /**
+     *
+     * @param column
+     * @return list
+     */
     List<Entity> getAllSorted(int column);
 }
